2ae021499fefef5b69bd6e808d9c7a40afd0e6a9
Daniel Lowinger <daniel.lowinger@post.runi.ac.il>
2024-03-31 16:44:56 +0300

    add code for Coins

--- Coins.java
+++ Coins.java
@@ -4,6 +4,12 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		// gets a number of cents and prints how to represent this quantity using as
+		// many quarters as possible plus
+		// the remainder in cents.
+		int a = Integer.parseInt(args[0]);
+		int quarters = a / 25;
+		int cents = a % 25;
+		System.out.println("use " + quarters + " quarters " + "and " + cents + " cents");
 	}
 }
